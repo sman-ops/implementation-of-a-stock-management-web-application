44dc1768e346f70a9ec7ad6b71061242fcae6417
sman-ops <slimen.ghenimi@gmail.com>
2023-06-06 22:28:46 +0100

    create all entities in spring

---
+++ backend/src/main/java/com/spring/restaurant/model/AbstractEntity.java
@@ -0,0 +1,41 @@
+package com.spring.restaurant.model;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity implements Serializable {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@CreatedDate
+	@Column(name="creationDate", nullable = false)
+	@JsonIgnore
+	private Instant creationDate;
+	
+	@LastModifiedDate
+	@Column(name="lastModifiedDate")
+	@JsonIgnore
+	private Instant lastModifiedDate;
+	
+
+}
