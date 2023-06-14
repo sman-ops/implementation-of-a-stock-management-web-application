4ae56f3195990da30b7e5affaf7d5142ce67a538
sman-ops <slimen.ghenimi@gmail.com>
2023-06-14 21:15:55 +0100

    finish the articleServiceImpl

--- backend/src/main/java/com/spring/restaurant/repository/ArticleRepository.java
+++ backend/src/main/java/com/spring/restaurant/repository/ArticleRepository.java
@@ -1,5 +1,7 @@
 package com.spring.restaurant.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.spring.restaurant.model.Article;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Integer>{
+	
+	Optional<Article> findArticleByCodeArticle(String codeArticle);
 
 }
--- backend/src/main/java/com/spring/restaurant/services/Impl/ArticleServiceImpl.java
+++ backend/src/main/java/com/spring/restaurant/services/Impl/ArticleServiceImpl.java
@@ -1,11 +1,21 @@
 package com.spring.restaurant.services.Impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.spring.restaurant.dto.ArticleDto;
+import com.spring.restaurant.exception.EntityNotFoundException;
+import com.spring.restaurant.exception.ErrorCodes;
+import com.spring.restaurant.exception.InvalidEntityException;
+import com.spring.restaurant.model.Article;
+import com.spring.restaurant.repository.ArticleRepository;
 import com.spring.restaurant.services.ArticleService;
+import com.spring.restaurant.validator.ArticleValidator;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,29 +23,69 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class ArticleServiceImpl implements ArticleService {
+	
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	
 
 	@Override
 	public ArticleDto save(ArticleDto dto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		List<String> errors = ArticleValidator.validate(dto);
+		if(!errors.isEmpty()) {
+			log.error("Article  is not valid {}",dto);
+			throw new InvalidEntityException("Article n'est pas valide",ErrorCodes.ARTICLE_NOT_VALID,errors);
+		}
+		
+		// Article  savedArticle = articleRepository.save(articleDto.toEntity(dto));
+		
+		 return ArticleDto.fromEntity(
+			        articleRepository.save(
+			            ArticleDto.toEntity(dto)
+			        )
+			    );
+			  }
 
 	@Override
 	public ArticleDto findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(id == null) {
+			log.error("Article ID is null ");
+			return null;
+		}
+			
+		Optional<Article> article = articleRepository.findById(id);
+		
+		return  Optional.of(ArticleDto.fromEntity(article.get())).orElseThrow(()->
+		 new EntityNotFoundException(
+		            "Aucun article avec l'ID = " + id + " n' ete trouve dans la BDD",
+		            ErrorCodes.ARTICLE_NOT_FOUND)
+		);
 	}
 
 	@Override
 	public ArticleDto findByCodeArticle(String codeArticle) {
-		// TODO Auto-generated method stub
-		return null;
+		 if (!StringUtils.hasLength(codeArticle)) {
+		      log.error("Article CODE is null");
+		      return null;
+		    }
+		 
+		 Optional<Article> article = articleRepository.findArticleByCodeArticle(codeArticle);
+			
+			return  Optional.of(ArticleDto.fromEntity(article.get())).orElseThrow(()->
+			 new EntityNotFoundException(
+			            "Aucun article avec le code = " + codeArticle + " n' ete trouve dans la BDD",
+			            ErrorCodes.ARTICLE_NOT_FOUND)
+			);
+
 	}
 
 	@Override
 	public List<ArticleDto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		 return articleRepository.findAll().stream()
+			        .map(ArticleDto::fromEntity)
+			        .collect(Collectors.toList());
 	}
 
 	@Override
@@ -46,7 +96,13 @@ public class ArticleServiceImpl implements ArticleService {
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
+		  if (id == null) {
+		      log.error("Article ID is null");
+		   
+		      return;
+		    }
+		  
+		  articleRepository.deleteById(id);
 		
 	}
 
